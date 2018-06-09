ade07ba9957e937fd2fbb9444a6e6ea8828453a5
Sergio Raúl <sergioraul92@gmail.com>
2018-06-09 11:17:20 +0200

    Primer comit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+Vue.component('todo-list', {
+   template: '#todo-template',
+    
+    data: function(){
+        return{
+            nuevaTarea: null,
+            editandoTarea : null,              
+        }           
+    },    
+    props: ['tareas'],
+    methods:{
+        agregarTarea: function(tarea){
+            this.tareas.unshift({
+                titulo: tarea, completado: 'false'
+            });
+            this.nuevaTarea = '';            
+        },
+        
+        editarTarea: function(tarea){
+            console.info(tarea);
+        },
+        
+        eliminarTarea: function(indice){
+            this.tareas.splice(indice, 1);
+        },
+    }
+});
+
+new Vue({
+    el: '#main',
+    
+    data:{             
+        tareas:[
+            {titulo:'Salir a correr', completado: false},
+            {titulo:'Ir al gimnasio', completado: false},
+            {titulo:'Lavar el coche', completado: false},
+            {titulo:'Hacer la compra', completado: false}        
+        ]
+    }  ,  
+});
